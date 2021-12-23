ebb8aa85eafc8c64a386e2a28dc2119361906ece
FedorovAleksandr1 <sasha1977335@gmail.com>
2021-12-24 02:19:42 +0300

    comment

---
+++ cmake-build-debug/CMakeFiles/untitled51.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled51.dir/main.cpp.obj"
+  "libuntitled51.dll.a"
+  "untitled51.exe"
+  "untitled51.exe.manifest"
+  "untitled51.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled51.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/untitled51.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for untitled51.
+# This may be replaced when dependencies are built.
