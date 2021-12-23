e56e4c2856f4b3be29adb0960ba34ebd1c11aa3e
FedorovAleksandr1 <sasha1977335@gmail.com>
2021-12-24 02:26:13 +0300

    TestForLab3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
-#include "pch.h"
-TEST(TestCaseName, TestName) {
-EXPECT_EQ(1, 1);
-EXPECT_TRUE(true);
+#include <gtest/gtest.h>
+#include <iostream>
+
+class Foo
+{
+public:
+    Foo()
+            : i(0)
+    {
+        std::cout << "CONSTRUCTED" << std::endl;
+    }
+    ~Foo()
+    {
+        std::cout << "DESTRUCTED" << std::endl;
+    }
+    int i;
+};
+
+class TestFoo : public ::testing::Test
+{
+protected:
+    void SetUp()
+    {
+        foo = new Foo;
+        foo->i = 5;
+    }
+    void TearDown()
+    {
+        delete foo;
+    }
+    Foo *foo;
+};
+
+TEST_F(TestFoo, test1)
+{
+ASSERT_EQ(foo->i, 5);
+foo->i = 10;
 }
-TEST(MoreVal1, MoreVal1) {
-EXPECT_EQ(99, 99);
-EXPECT_TRUE(true);
+
+TEST_F(TestFoo, test2)
+{
+ASSERT_EQ(foo->i, 5);
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
